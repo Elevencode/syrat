5ff319f4ca458d1cc84456bf9b57f525f2851c29
elevencode <archcores@gmail.com>
2022-05-15 14:02:43 +0500

    initial

---
+++ android/app/src/main/kotlin/com/example/syrat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.syrat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SyratApp());
+}
+
+class SyratApp extends StatefulWidget {
+  const SyratApp({Key? key}) : super(key: key);
+
+  @override
+  _SyratAppState createState() => _SyratAppState();
+}
+
+class _SyratAppState extends State<SyratApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
